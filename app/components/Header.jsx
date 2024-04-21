e7cc58e8ddde17223d3626f68367c362437e9d43
Bryan Bayoca <123434159+binayoca686@users.noreply.github.com>
2024-04-21 17:54:24 +0800

    week3

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,34 +1,20 @@
-// 'use client';
-// import { usePathname } from 'next/navigation'
+'use client';
+import { usePathname } from 'next/navigation';
 import Head from "next/head";
-import Script from "next/script";
-
 
 function Header() {
-  // const pathname = usePathname();
-  // const shouldDisplayBreadcrumb = pathname.includes("week");
-  // const weekNumber = pathname.substring(pathname.lastIndexOf('/') + 1);
+  const pathname = usePathname();
+  const shouldDisplayBreadcrumb = pathname.includes("week");
+  const weekNumber = pathname.substring(pathname.lastIndexOf('/') + 1);
   return (
     <>
     <Head>
     <link rel="shortcut icon" href="../public/img/BryanLogoB.png" />
     </Head>
-    <Script src="lib/jquery/jquery.min.js"/>
-    <Script src="lib/jquery/jquery-migrate.min.js"/>
-    <Script src="lib/popper/popper.min.js"/>
-    <Script src="lib/bootstrap/js/bootstrap.min.js"/>
-    <Script src="lib/easing/easing.min.js"/>
-    <Script src="lib/counterup/jquery.waypoints.min.js"/>
-    <Script src="lib/counterup/jquery.counterup.js"/>
-    <Script src="lib/owlcarousel/owl.carousel.min.js"/>
-    <Script src="lib/lightbox/js/lightbox.min.js"/>
-    <Script src="lib/typed/typed.min.js"/>
-    <Script src="contactform/contactform.js"/>
-    <Script src="js/main.js"/>
     
     <nav class="navbar navbar-b navbar-trans navbar-expand-md fixed-top" id="mainNav">
 
-      {/* {shouldDisplayBreadcrumb ? (
+      {shouldDisplayBreadcrumb ? (
         <>
           <div class="container">
             <a className="navbar-brand js-scroll mx-auto" href="/">
@@ -53,12 +39,12 @@ function Header() {
             </a>
           </div>
         </> 
-      )} */}
-      <div class="container">
+      )}
+      {/* <div class="container">
         <a className="navbar-brand js-scroll mx-auto" href="#page-top">
           <img src="img/BryanLogoW.png" style={{height: "150px"}}/> 
         </a>
-      </div>
+      </div> */}
     </nav>
     
     </>
