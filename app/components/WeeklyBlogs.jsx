a5ed28edb6b0536f569c5ae09a6e5356ea45260e
Bryan Bayoca <123434159+binayoca686@users.noreply.github.com>
2024-04-21 17:02:01 +0800

    week2

--- app/components/WeeklyBlogs.jsx
+++ app/components/WeeklyBlogs.jsx
@@ -23,6 +23,28 @@ function WeeklyBlogs() {
             </div>
           </a>
         </div>
+
+        <div class="col-md-4">
+          <a href="/week2">
+            <div class="card card-blog">
+              <div class="card-img">
+              <a href="/week2"><img src="img/week2/Screenshot (204).png" alt="" class="img-fluid"/></a>
+              </div>
+              <div class="card-body">
+                <div class="card-category-box">
+                  <div class="card-category">
+                    <h6 class="category">Week 2</h6>
+                  </div>
+                </div>
+                <h3 class="card-title"><a href="/week2">See more about Week 2</a></h3>
+                <p class="card-description">
+                  This pertains for dates October 09, 2023 to October 13, 2023, where I going through Udemy course and learning stuffs.
+                </p>
+              </div>
+              
+            </div>
+          </a>
+        </div>
         
       </div>
   )
---
+++ app/week2/page.js
@@ -0,0 +1,69 @@
+
+import React from 'react'
+import Header from '../components/Header'
+
+
+function page() {
+
+  return (
+    <>
+    <Header/>
+
+  <div id="home" class="intro route bg-image" style={{backgroundImage: "url(img/intro-bg3.jpg)", height: "210px"}}>
+    <div class="overlay-itro"></div>
+    <div class="intro-content display-table">
+      
+    </div>
+  </div>
+  <section class="blog-wrapper sect-pt4" id="blog">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="post-box">
+            <blockquote class="blockquote">
+              <div class="post-meta">
+                <h1 class="article-title">Week 2, Finishing UP and Starting UP.</h1>
+              </div>
+            </blockquote>
+            <div class="post-thumb">
+              <img src="img/week1/Screenshot (240).png" class="img-fluid" alt=""/>
+            </div>
+
+            <div class="article-content">
+              <p>
+                Through slack, I was tasked or rather given 3 exercises to do, such as cloning other popular platforms like, <strong>Google Shopping, Slack, and Trello,</strong> using popular Javascript frameworks like <strong>React, through MERN stack and NEXT.js</strong>
+              </p>
+
+            <div class="post-thumb">
+              <img src="img/week1/Screenshot (203).png" class="img-fluid" alt=""/>
+            </div>
+              <p>
+                And also I was given opportunity to further expand my knowledge in coding by them providing me an UDEMY access to further expand my understanding about NEXTjs. So for this week I was busy finishing the udemy course which talks about the basics of nextjs, about compenents, factoring and other useful utilities that <strong>NPM</strong> provides
+              </p>
+
+            </div>
+          {/* next prev ======================*/}
+            <nav aria-label="...">
+              <ul class="pagination">
+                <li class="page-item disabled">
+                  <a class="page-link">Previous</a>
+                </li>
+                <li class="page-item">
+                  <a class="page-link" href="week2">Next</a>
+                </li>
+              </ul>
+            </nav>
+          {/* next prev ====================*/}
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+</>
+
+  )
+}
+
+export default page
